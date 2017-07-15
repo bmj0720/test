8b46676400c3240d56e2606a78a05a818db38972
包梦江 <baomengjiang@gmail.com>
2017-07-15 17:16:18 +0800

    333

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "tr22y1111www11111qq222pp"
+	c.Data["Website"] = "tr22y1111www11222111qq222pp"
 	c.Data["Email"] = "agaihghioooinjjjj@gmail.com"
 	c.TplName = "index.tpl"
 }
